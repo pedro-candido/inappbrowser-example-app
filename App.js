d20596f5593218cfe593e5974a38e0697041c6e4
Pedro Candido <pedroferreirac@hotmail.com>
2024-03-28 23:55:14 -0300

    feat: adding first url

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import InAppBrowser from "react-native-inappbrowser-reborn";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={InAppBrowser.open("https://google.com")}>
+        Open up App.js to start working on your app!
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
